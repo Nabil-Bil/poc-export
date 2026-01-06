7c07db347616a5977039515ca6bc84d14def0620
Nabil-Bil <n.radjai@outlook.com>
2026-01-06 18:31:31 +0100

    first commit

---
+++ src/main/java/poc_export/PocExportApplication.java
@@ -0,0 +1,13 @@
+package poc_export;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocExportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocExportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/poc_export/PocExportApplicationTests.java
@@ -0,0 +1,13 @@
+package poc_export;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocExportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
